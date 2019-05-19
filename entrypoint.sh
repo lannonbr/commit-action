7d100f6479dc8f79a99796a6c1541b7514737fd0
Benjamin Lannon <benjamin@lannonbr.com>
2019-05-19 11:34:05 -0400

    Empty email

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ date > $GITHUB_WORKSPACE/current_time.txt
 ls $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
 git config --global user.name "GitHub Actions Bot"
-git config --global user.email "a@example.com"
+git config --global user.email "<>"
 git add .
 git commit -m "Updating date"
 git push origin master
