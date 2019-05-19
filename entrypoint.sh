f03e5edd47e795ecdd8c87a478a9386739b28d2c
Benjamin Lannon <benjamin@lannonbr.com>
2019-05-19 11:29:38 -0400

    Updating entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ date
 date > $GITHUB_WORKSPACE/current_time.txt
 ls $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
+git config --global user.name "GitHub Actions Bot"
 git add .
 git commit -m "Updating date"
 git push origin master
